e1ab51df985c2e2218473a436095af02f2e3c94b
Loh Siu Yin <siuyin@beyondbroadcast.com>
2016-10-10 08:39:13 +0800

    Reduced capacity of hash.

--- randBytes.go
+++ randBytes.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	// R_S OMIT
 	rand.Seed(time.Now().UnixNano()) // seed random generator with current time
-	b := make([]byte, 5, 10)         // make a slice of 5 bytes with a capacity of 5
+	b := make([]byte, 5, 5)          // make a slice of 5 bytes with a capacity of 5
 	rand.Read(b)
 	fmt.Println(b)
 	s := base32.StdEncoding.EncodeToString(b) // Note: no 0 (zeros) in StdEncoding
